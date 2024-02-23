07b28685db457fab398debf9c1d404c16592b4e7
Natheer13 <natheer.shade@gmail.com>
2024-02-23 23:59:46 +0200

    Base Code

---
+++ src/main/java/za/ac/cput/Employee.java
@@ -0,0 +1,62 @@
+package za.ac.cput;
+public class Employee {
+
+    private String empNum;
+    private String name;
+    private String sName;
+    private double salary;
+
+    public Employee(){
+
+    }
+
+    public Employee(String empNum, String name, String sName, double salary) {
+        this.empNum = empNum;
+        this.name = name;
+        this.sName = sName;
+        this.salary = salary;
+    }
+
+    public String getEmpNum() {
+        return empNum;
+    }
+
+    public void setEmpNum(String empNum) {
+        this.empNum = empNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empNum='" + empNum + '\'' +
+                ", name='" + name + '\'' +
+                ", sName='" + sName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+}
