fd9395c591b797dee72972d6cf72b9d13e36c0bc
Chadwin Kyle Fritz <chadwinkyle2@gmail.com>
2024-02-24 16:01:59 +0200

    first commit

--- src/test/java/za/ac/cput/EmployeeTest.java
+++ src/test/java/za/ac/cput/EmployeeTest.java
@@ -1,12 +1,58 @@
 package za.ac.cput;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
 
+    private Employee employee1;
+    private Employee employee2;
+
     @BeforeEach
     void setUp() {
+        // Initialize Employee objects
+        employee1 = new Employee("E001", "Chadwin", "Fritz", 43000.0);
+        employee2 = new Employee("E002", "Natheer", "Shade", 44000.0);
+    }
+
+    @Test
+    void testGetEmpNum() {
+        assertEquals("E001", employee1.getEmpNum());
+        assertEquals("E002", employee2.getEmpNum());
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("Chadwin", employee1.getName());
+        assertEquals("Natheer", employee2.getName());
+    }
+
+    @Test
+    void testObjectEquality() {
+        Employee anotherEmployee = new Employee("E001", "Chadwin", "Fritz", 43000.0);
+        assertEquals(employee1, anotherEmployee);
+        assertNotEquals(employee1, employee2);
+    }
+
+    @Test
+    void testFailingCondition() {
+        assertNotEquals("E003", employee1.getEmpNum(), "This test intentionally fails because of a specific condition.");
+    }
+
+    @Disabled("This test is currently disabled for some reason.")
+    @Test
+    void testDisabledCondition() {
+        // Test code is currently disabled
+    }
+
+    @Test
+    void testToString() {
+        String expectedToString1 = "Employee{empNum='E001', name='Chadwin', sName='Fritz', salary=43000.0}";
+        String expectedToString2 = "Employee{empNum='E002', name='Natheer', sName='Shade', salary=44000.0}";
+        assertEquals(expectedToString1, employee1.toString());
+        assertEquals(expectedToString2, employee2.toString());
     }
 }
